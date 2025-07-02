9bd80123bc55be178e1f8761c312f59d431aa79e
Dzianis Shapel <dshapel@alfabank.ru>
2025-07-03 00:08:24 +0300

    hints

--- src/main/java/eu/senla/management/rest/GetToken.java
+++ src/main/java/eu/senla/management/rest/GetToken.java
@@ -4,6 +4,15 @@ import io.restassured.response.ValidatableResponse;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+/**
+* 1. Переименуй класс (но это не самое важное)
+ * 2 Тебе надо описать два запроса
+ * Первый запрос просто получает токен
+ * Второй запрос получает авторазационную куку которую ты должна подкинуть в браузер
+ * Итак
+*/
+
+
 public class GetToken {
     public static String token;
     public static String cookie;
@@ -28,7 +37,47 @@ public class GetToken {
 
         return new CookieRequest (token,cookie);
 
-
-
     }
+
+
+    // ЗАПРОС НОМЕР 1
+/*    private static void getTokenFromLoginPage(){
+        Response response =  given().
+                spec(getRequestSpecification()).
+                when().
+                get(LOGIN).
+                then().spec(getResponseSpecification()).
+                extract().
+                response();
+        cookie = response.detailedCookie("orangehrm");
+        Document doc = Jsoup.parse(
+                response.body().prettyPrint()
+        );
+        Element element  = doc.selectFirst("auth-login");
+        token = element.attr("token");
+    }*/
+
+    //запрос 2
+   // Обрати внимание что здесь в строке 63 ты вызываешь первый запрос, инициализируешь токен и потом его передаешь в обмен на куки
+    // это кстати можно отрефакторить и сделать по-своему
+    // в целом ты получишь куки ее то и надо положить в браузер во время его инициализации
+    // НО етьс хитрость, что ты получила Cookie из рест ашурда
+    // а у селениума свой тип Cookie
+/*   public static Cookie getAuthCookie(){
+        getTokenFromLoginPage();
+        HashMap<String, String> formParams = new HashMap<>();
+        formParams.put("_token", token);
+        formParams.put("username", ConfigLoader.getInstance().getUsername());
+        formParams.put("password", ConfigLoader.getInstance().getPassword());
+        return given().
+                spec(getRequestSpecification()).
+                cookie(cookie).
+                formParams(formParams).
+                when().
+                post(VALIDATE).
+                then().spec(getResponseSpecification()).
+                statusCode(302).
+                extract().
+                response().detailedCookie("orangehrm");
+    }*/
 }
