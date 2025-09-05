692948db275e19e79d096784a20a878b05387f47
Luboval <Lyubov.Kalosha@nedra.digital>
2025-09-05 11:18:03 +0300

    add listener for allure

--- src/main/java/eu/senla/listeners/ScreenshotListener.java
+++ src/main/java/eu/senla/listeners/ScreenshotListener.java
@@ -2,10 +2,18 @@ package eu.senla.listeners;
 
 import eu.senla.management.common.Driver;
 import eu.senla.management.common.ScreenshotUtil;
+import io.qameta.allure.Attachment;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 
 @Slf4j
 public class ScreenshotListener implements ITestListener {
@@ -19,4 +27,20 @@ public class ScreenshotListener implements ITestListener {
         }
     }
 
+    @Attachment(value = "Screenshot", type = "image/png")
+    public static byte[] takeScreenshot(WebDriver driver) {
+
+        byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        // save to disk
+        try {
+            File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+            File dest = new File("build/reports/tests/test/screenshots/" + timestamp + ".png");
+            dest.getParentFile().mkdirs();
+            Files.copy(screenshotFile.toPath(), dest.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return screenshotBytes;
+    }
 }
